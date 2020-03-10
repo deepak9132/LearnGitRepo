2b02a5b11ef83f70f3aee00a5a1752db877ccfb4
Deepak Kumar <deepakkumar55291@gmail.com>
2020-03-10 12:51:15 +0530

    updated for_loops with new algos

--- ForLoops.java
+++ ForLoops.java
@@ -1,7 +1,8 @@
 public class ForLoops{
 	public static void main(String args[]){
 		for(int i=0;i<5;i++){
-			System.out.println("Hello");
+			System.out.print(i+" ");
 		}
+		System.out.println("Done with for loops");
 	}
 }
